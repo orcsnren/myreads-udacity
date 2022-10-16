4dac5b8b06df72c66240349cb4c4169da76af186
Soner <sonereren39@gmail.com>
2022-10-16 03:21:47 +0300

    refactor - List books section moved to ListBooks component

---
+++ src/components/ListBooks.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import BookShelf from './BookShelf'
+import { Link } from 'react-router-dom';
+
+const ListBooks = ({ filterBooks , updateBook}) => (
+
+    <div className="list-books">
+        <div className="list-books-title">
+            <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+            <div>
+                <BookShelf bookShelfTitle="Current Readings" books={filterBooks('currentlyReading')} updateBook={updateBook} />
+                <BookShelf bookShelfTitle="Want to Read" books={filterBooks('wantToRead')} updateBook={updateBook} />
+                <BookShelf bookShelfTitle="Read" books={filterBooks('read')} updateBook={updateBook} />
+            </div>
+        </div>
+        <div className="open-search">
+            <Link to="search">
+                <button>Add a Book</button>
+            </Link>
+        </div>
+    </div>
+
+)
+export default ListBooks;
